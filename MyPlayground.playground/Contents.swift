91956794b15c2ab474eb8cf687d89810270d9f3c
LAWN <user.lawn@gmail.com>
2022-03-26 17:57:29 +0900

    learn float, doble

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,36 +2,35 @@ import UIKit
 
 
 //상수와 변수에 대해 공부합니다.
-/*let constant: String = "차후에 변경할 수 없습니다."
-var variable: String = "차후에 변경이 가능합니다."
-
-print(constant)
-print(variable)
-*/
-
+//let constant: String = "차후에 변경할 수 없습니다."
+//var variable: String = "차후에 변경이 가능합니다."
+//
+//print(constant)
+//print(variable)
+//
 //print 와 dump에 대해 공부합니다
-/* struct BasicInformation {
-    let name: String
-    var age: Int
-}
+//struct BasicInformation {
+//    let name: String
+//    var age: Int
+//}
+//
+//var yagomInfo: BasicInformation = BasicInformation(name: "yagom", age: 99)
+//
+//class Person {
+//    var height: Float = 0.0
+//    var weight: Float = 0.0
+//}
+//
+//let yagom: Person = Person()
+//yagom.height = 182.5
+//yagom.weight = 78.5
+//
+//print(yagomInfo)
+//dump(yagomInfo)
+//
+//print(yagom)
+//dump(yagom)
 
-var yagomInfo: BasicInformation = BasicInformation(name: "yagom", age: 99)
-
-class Person {
-    var height: Float = 0.0
-    var weight: Float = 0.0
-}
-
-let yagom: Person = Person()
-yagom.height = 182.5
-yagom.weight = 78.5
-
-print(yagomInfo)
-dump(yagomInfo)
-
-print(yagom)
-dump(yagom)
-*/
 
 //보간법에 대해 공부합니다.
 //let name: String = "yagom"
@@ -44,4 +43,10 @@ dump(yagom)
 //let isTimeUnlimited: Bool = false
 //print("시간은 무한합니까?: \(isTimeUnlimited)")
 
+//float과 Double에 대해서 공부합니다
+//var floatValue: Float = 1234567890.1
+//
+//let doubleValue: Double = 1234567890.1
+//
+//print("floatValue: \(floatValue) doubleValue: \(doubleValue)")
 
