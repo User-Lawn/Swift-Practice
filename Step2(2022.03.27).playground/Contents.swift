a98d3987e5c4570184f0e9a94687658c1aa59074
LAWN <user.lawn@gmail.com>
2022-03-27 19:06:40 +0900

    learn rawValue

--- Step2(2022.03.27).playground/Contents.swift
+++ Step2(2022.03.27).playground/Contents.swift
@@ -4,20 +4,24 @@ var greeting = "Hello, playground"
 
 //열거형
 
-enum School {
-    case primary
-    case elementary
-    case middle
-    case high
-    case college
+enum School: String {
+    case primary = "유치원"
+    case elementary = "초등학교"
+    case middle = "중학교"
+    case high = "고등학교"
+    case college = "대학"
 }
 
 //enum School {case primary, elementary, middle, high, college}
 
-var highestEducationLevel: School = School.middle
+let highestEducationLevel: School = School.middle
+print("저의 최종학력은 \(highestEducationLevel.rawValue)입니다")
 
 //var highestEducationLevel: School = .middle
 
-highestEducationLevel = .high
+enum WeekDays: Character {
+    case mon = "월", tue = "화", wed = "수"
+}
 
-
+let today: WeekDays = WeekDays.wed
+print("오늘은\(today.rawValue)요일입니다.")
