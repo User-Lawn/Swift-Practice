7605e1cacbe0cb2c0ff0a8f4671b2f9a84d9a777
이준영 <74142881+User-Lawn@users.noreply.github.com>
2022-04-22 00:09:25 +0900

    코더스하이 IOS 앱 만들기 입문

--- CodersHI-IOS Basic/13 QuestionBot/QuestionBot/QuestionBot/QuestionAnswerer.swift
+++ CodersHI-IOS Basic/13 QuestionBot/QuestionBot/QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,22 @@
 struct MyQuestionAnswerer {
     func responseTo(question: String) -> String {
-        // TODO: Write a response 
-        return "?"
+       
+        let lowerQuestion = question.lowercased()
+        
+        if lowerQuestion == "where are the cookies?"{
+            return "To the cookie jar!"
+        } else if lowerQuestion.hasPrefix("where"){
+            return "To the North"
+        } else {
+            
+            let defaultNumber = question.count % 2
+            
+            if defaultNumber == 0{
+                return "That really depends"
+            } else {
+                return "Ask me again tomorrow"
+            }
+        }
+       
     }
 }
